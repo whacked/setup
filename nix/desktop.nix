8b186ebcb39219e660d9790d71c9ad5612d3b7d2
natto <whacked@users.noreply.github.com>
2019-01-13 16:18:34 -0800

    Update desktop.nix for desktop bound software

--- nix/desktop.nix
+++ nix/desktop.nix
@@ -1,8 +1,26 @@
 with import <nixpkgs> {};
 [
-    haskellPackages.xmonad
-    haskellPackages.xmonad-extras
-    haskellPackages.xmonad-contrib
+    # fixes Gtk-WARNING "Locale not supported by C library"
+    glibcLocales
+
+    haskellPackages.ghc
+    ## leave this to cabal for now
+    # haskellPackages.xmonad
+    # haskellPackages.xmonad-extras
+    # haskellPackages.xmonad-contrib
+    ## these are required to build xmonad from cabal
+    ## order is important :-(
+    # cabal install alex happy c2hs
+    alsaLib
+    x11
+    pkgconfig
+    xorg.libXext
+    xorg.libXrandr
+    xorg.libXinerama
+    xorg.libXScrnSaver
+    haskellPackages.cabal-install
+
     xorg.xkbcomp
     xorg.xmodmap
+    xorg.xmessage
 ]
