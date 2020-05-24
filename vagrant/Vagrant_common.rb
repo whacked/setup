4ccfac094644bbc4c1265d67c0540eb81dff45a6
natto <whacked@users.noreply.github.com>
2020-05-24 08:26:33 -0700

    update vagrant common rb to handle more candidate pkey paths

--- vagrant/Vagrant_common.rb
+++ vagrant/Vagrant_common.rb
@@ -1,5 +1,28 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-PKEY_PATH = File.join ENV["HOME"], ".ssh", "id_rsa"
-warn("No pkey found at #{PKEY_PATH}") if not File.exists? PKEY_PATH
+$pkey_candidate_paths = [
+    "/c/Users/#{ENV['USER']}/.ssh/id_rsa",
+    "C:\\Users\\#{ENV['USER']}\\.ssh\\id_rsa",
+    File.join(ENV["HOME"], ".ssh", "id_rsa"),
+]
+
+def get_pkey_path()
+    for candidate in $pkey_candidate_paths do
+        if File.exists? candidate then
+            puts("found pkey at #{candidate}")
+            return candidate
+        end
+    end
+    return nil
+end
+
+PKEY_PATH = get_pkey_path
+if PKEY_PATH.nil? then
+    puts("no key found! tried these:")
+    for candidate in $pkey_candidate_paths do
+        puts("- #{candidate}")
+    end
+    abort()
+end
+
