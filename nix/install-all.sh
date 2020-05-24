6cf0b20ac0a279b7dba533ecccee4cfcfa24d070
natto <whacked@users.noreply.github.com>
2020-05-24 14:24:14 -0700

    add direct nix-env -i install facilitator script

---
+++ nix/install-all.sh
@@ -0,0 +1,7 @@
+_matching_files=$(cat config.nix | grep import | sed 's|.\+import \(.\+nix\).*|\1|')
+_packages=$(cat $_matching_files | awk '/^\[/,EOF' | grep '^ \+[_a-zA-Z0-9]\+$' | sort)
+echo "=== MATCHING FILES ==="
+echo $_matching_files
+echo "=== PACKAGES ==="
+echo $_packages | tr ' ' '\n'
+nix-env -i $_packages
