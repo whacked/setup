e2b4d6a56c420fede83f75ad162167d106a0f072
whacked <whacked@users.noreply.github.com>
2020-09-07 21:47:50 -0700

    update gitignore generator to refresh from exsting "### src:" blocks

--- bash/git_shortcuts.sh
+++ bash/git_shortcuts.sh
@@ -1,14 +1,23 @@
+function _read_gitignore_source() {
+    SOURCE_URL=$1
+    echo "### src: $SOURCE_URL"
+    curl -s -L $SOURCE_URL
+    echo
+}
+
 function generate-gitignore() {
     LOCAL_GITIGNORE_FILE=.gitignore.local
+    existing_sources=$(cat .gitignore | grep '^#\+ src: ' | sed -e 's|^#\+ src: ||')
     \rm .gitignore
     echo '### AUTO-POPULATED. DO NOT EDIT' > .gitignore
     echo "### user ignores should go in $LOCAL_GITIGNORE_FILE" >> .gitignore
     echo >> .gitignore
+    for existing_source in ${existing_sources[@]}; do
+        _read_gitignore_source $existing_source >> .gitignore
+    done
     for f in $@; do
         SOURCE_URL=https://raw.githubusercontent.com/github/gitignore/master/${f%.gitignore}.gitignore
-        echo "### src: $SOURCE_URL" >> .gitignore
-        curl -s -L $SOURCE_URL >> .gitignore
-        echo >> .gitignore
+        _read_gitignore_source $SOURCE_URL >> .gitignore
     done
     if [ -e .gitignore.local ]; then
         echo "### $LOCAL_GITIGNORE_FILE" >> .gitignore
