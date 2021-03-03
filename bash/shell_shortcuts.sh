b96719d111aa4be5d3875aa27ba4c1a9958f762e
whacked <whacked@users.noreply.github.com>
2021-03-03 11:38:35 -0800

    add bash stacktrace fn + var utils

--- bash/shell_shortcuts.sh
+++ bash/shell_shortcuts.sh
@@ -45,3 +45,43 @@ record-output-change() {
     done
 }
 
+function stacktrace() {
+    ### ref https://gist.github.com/akostadinov/33bb2606afe1b334169dfbf202991d36
+    local i message="${1:-""}"
+    local stack_size=${#FUNCNAME[@]}
+    # to avoid noise we start with 1 to skip the get_stack function
+    for (( i=1; i<$stack_size; i++ )); do
+        local func="${FUNCNAME[$i]}"
+        [ x$func = x ] && func=MAIN
+        local linen="${BASH_LINENO[$(( i - 1 ))]}"
+        local src="${BASH_SOURCE[$i]}"
+        [ x"$src" = x ] && src=non_file_source
+        printf "  - %d:  %-30s %s:%d\n" $i "$func(...)" $src $linen
+    done
+}
+
+function assert-nargs() {
+    parent_nargs=$(( $# - 1 ))
+    if [ $parent_nargs -ne $1 ]; then
+        red=`tput setaf 1`
+        reset=`tput sgr0`
+        echo "ERROR:  $red${FUNCNAME[1]}()$reset  needs $1 arguments; got $parent_nargs"
+        echo "  stack (most recent call first)"
+        stacktrace
+        return -1
+    fi
+}
+
+function export-var-or-default() {
+    assert-nargs 2 $* || return
+    var_name=$1
+    var_value=${!var_name}
+    default_value=$2
+    if [ "x$var_value" == "x" ]; then
+        echo "WARNING: using default value for variable: $(tput setaf 2)$var_name$(tput sgr0)=$default_value"
+        export $var_name=$default_value
+    else
+        :
+    fi
+}
+
