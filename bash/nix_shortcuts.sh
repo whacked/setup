04714b9dea40384568b23c21464093bbcc347d92
whacked <whacked@users.noreply.github.com>
2021-04-02 10:38:28 -0700

    change shortcut printer to echo relpath

--- bash/nix_shortcuts.sh
+++ bash/nix_shortcuts.sh
@@ -6,8 +6,9 @@ fi
 _SHORTCUTS_HELP=${_SHORTCUTS_HELP-}
 function echo-shortcuts() {
     target_file=$(realpath ${1-*.nix})
+    target_relpath=$(realpath --relative-to=$PWD $target_file)
     help_string=
-    help_string="$help_string=== shortcuts from $target_file ===\n"
+    help_string="$help_string=== shortcuts from $target_relpath ===\n"
     help_string="$help_string"'\033[0;33m'$(cat $target_file | grep --color '^\s*\([a-z][-a-zA-Z0-9]*()\|function [a-zA-Z]\).\+')'\033[0m'
     help_string="$help_string"'\n\033[0;35m'$(cat $target_file | grep --color '^\s*\(alias\).\+')'\033[0m'
     _SHORTCUTS_HELP="${_SHORTCUTS_HELP}$help_string\n"
