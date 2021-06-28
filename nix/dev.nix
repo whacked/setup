9c8837aa4d53a27d6464b700a685e59f8359ee9b
natto <natto@fixme.example.com>
2021-06-28 09:57:48 -0700

    add pinned package overlay for swfdump (latest marked broken)

--- nix/dev.nix
+++ nix/dev.nix
@@ -1,5 +1,15 @@
 # dev
 with import <nixpkgs> {};
+
+let
+  pinnedNixPkgs = import (fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "8c5d37129fc5097d9fb52e95fb07de75392d1c3c";
+    sha256 = "02ahrhr1s7b2g0x1xqhyyk1kk7x0q5nifddnih7pckcmkkxcgiip";
+  }) {};
+  swftools = pinnedNixPkgs.swftools;
+in
 [
     R
     cairo
