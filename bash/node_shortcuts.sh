82198aaa40ae3734fe5f40392c9727087ab5327f
whacked-xps <whacked@users.noreply.github.com>
2021-07-22 10:42:02 +0800

    add handy package.json differ for package refactoring

--- bash/node_shortcuts.sh
+++ bash/node_shortcuts.sh
@@ -7,3 +7,10 @@ activate-node-env() {
     fi
 }
 
+diff-package-json() {
+    assert-nargs 1+ $* || return
+    # diff current package.json against the output of a package.json generator jsonnet file
+    compare_jsonnet=$1
+    source_json=${2-package.json}
+    icdiff -N <(jsonnet $source_json | jq -S) <(jsonnet $compare_jsonnet | jq -S)
+}
