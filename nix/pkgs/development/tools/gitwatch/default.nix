5c1ed71fe03066f8e7adb69a77b4875b5797b080
whacked-xps <whacked@users.noreply.github.com>
2022-03-22 17:12:12 +0800

    update gitwatch

--- nix/pkgs/development/tools/gitwatch/default.nix
+++ nix/pkgs/development/tools/gitwatch/default.nix
@@ -1,6 +1,5 @@
-# for nix-shell
+# for nix-shell, uncomment `with`, comment out { lib ... }, and nix-shell from cwd
 # with import <nixpkgs> {};
-
 { lib, stdenv, fetchzip, inotify-tools, makeWrapper }:
 
 stdenv.mkDerivation rec {
@@ -9,7 +8,7 @@ stdenv.mkDerivation rec {
 
   src = fetchzip {
     url="https://github.com/gitwatch/gitwatch/archive/master.zip";
-    sha256="1jrjvzi3lka05dv0g4jswajak2w267995fcvka4bnkfik71k8kmv";
+    sha256="1n78iffrdpmzrkbr1hbip41g9ifcwi9f9gygfifdw19q7v842bqp";
   };
 
   buildInputs = [
