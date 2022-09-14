1987b6a751b951a2ead08d3d03849d311ad8dcf2
whacked-xps <whacked@users.noreply.github.com>
2022-09-14 15:31:24 +0800

    add package def for sqlitefs

---
+++ nix/pkgs/misc/sqlitefs/default.nix
@@ -0,0 +1,33 @@
+{ rustPlatform, stdenv, pkgs }:
+
+rustPlatform.buildRustPackage rec {
+  pname = "sqlitefs";
+  version="0.1.0";
+  goPackagePath = "github.com/narumatt/sqlitefs";
+
+  buildInputs = [
+    pkgs.fuse
+    pkgs.pkgconfig
+  ];  # join lists with ++
+
+  nativeBuildInputs = [
+    pkgs.fuse
+    pkgs.pkgconfig
+  ];
+
+  src = pkgs.fetchFromGitHub {
+    owner = "narumatt";
+    repo = "sqlitefs";
+    rev = "master";
+    sha256 = "03mk7q47smbxj86qsilz0yh7kbw0yb6pfw0ypnrn16xiavwdbq7s";
+  };
+
+  cargoSha256 = "0z82zgkg6n3vwy8plhik8zw3q898xdg0m2mx8kddp5an2f7nji25";
+
+  meta = with pkgs.lib; {
+    description = ''read-write enabled fuse mountable sqlitedb'';
+    downloadPage = "https://github.com/narumatt/sqlitefs";
+    inherit version;
+  };
+}
+
