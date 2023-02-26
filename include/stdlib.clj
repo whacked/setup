49a7383d01cbf87a198e61474deb31fea96d4763
whacked-xps <whacked@users.noreply.github.com>
2023-02-26 10:38:44 +0800

    add time formatter / timestamp to babashka local stdlib

--- include/stdlib.clj
+++ include/stdlib.clj
@@ -2,7 +2,18 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
             [babashka.pods :as pods]
-            [hiccup2.core :as h]))
+            [hiccup2.core :as h])
+
+  (:import java.time.format.DateTimeFormatter
+           java.time.LocalDateTime))
+
+
+(def formatter (DateTimeFormatter/ofPattern "yyyy-MM-dd HH:mm:ss"))
+
+
+(defn timestamp-string []
+  ;; => "2020-07-18 18:04:04"
+  (.format (LocalDateTime/now) formatter))
 
 (defn ->html [hiccup]
   (str (h/html {:escape-strings? false} hiccup)))
