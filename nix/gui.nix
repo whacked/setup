ddb65b43b400ea23221dbdac52dbf92ad0c25a06
whacked-xps <whacked@users.noreply.github.com>
2024-06-19 23:59:29 +0800

    pin nixpkgs to load old veracrypt (1.25.9) for truecrypt support
    veracrypt 1.26.7 drops support for truecrypt, so we need to load the
    earlier one

--- nix/gui.nix
+++ nix/gui.nix
@@ -1,5 +1,17 @@
 # desktop
 { pkgs, ... }:
+
+let
+  # veracrypt dropped truecrypt volume support in the latest version (1.26.7)
+  # pin it to 23.11 for now
+  pinnedNixPkgs = import (pkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "057f9aecfb71c4437d2b27d3323df7f93c010b7e";
+    hash = "sha256-MxCVrXY6v4QmfTwIysjjaX0XUhqBbxTWWB4HXtDYsdk=";
+  }) {};
+  veracrypt = pinnedNixPkgs.veracrypt;
+in
 with pkgs; [
     conky
     hack-font
