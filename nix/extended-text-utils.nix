ccd87fa3cbc1d27805e0f0851b99b47d72c16882
whacked <whacked@users.noreply.github.com>
2026-05-17 11:52:45 +0800

    comment out beads-rust default inclusion

--- nix/extended-text-utils.nix
+++ nix/extended-text-utils.nix
@@ -4,12 +4,13 @@
 let
   ck         = pkgs.callPackage (import ./pkgs/development/tools/ck/default.nix)  {};
   tuitab     = pkgs.callPackage (import ./pkgs/misc/tuitab/default.nix)           {};
-  tidyViewer = pkgs.callPackage (import ./pkgs/misc/tidy-viewer/default.nix)               {};
+  tidyViewer = pkgs.callPackage (import ./pkgs/misc/tidy-viewer/default.nix)      {};
   beadsRust  = pkgs.callPackage (import ./pkgs/misc/beads-rust/default.nix)       {};
 in
 with pkgs; [
   tuitab
-  beadsRust
+  # deletion candidate 2026-05-17: unclear if fits workflow
+  # beadsRust
   tidyViewer
 ]
 # ck linux build currently broken; darwin only
